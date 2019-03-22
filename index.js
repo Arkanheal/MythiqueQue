6568a71a14fb14400b5353fd21441c427821d1d3
BejaouiKhalil <35938684+BejaouiKhalil@users.noreply.github.com>
2019-03-22 22:20:08 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,60 +4,55 @@ const bot = new discord.Client();
 const prefix =('+');
 const chan_bot = bot.channels.find('name', 'mythicqueue');
 
-var dps=[];
-var tank=[];
+var Dps=[];
+var Tank=[];
 var heal=[];
 
 bot.on('ready',()=> {
     console.log(`logged in  as ${bot.user.username} !`);
-
-    const message_intro = channel.send('Join queue for MM dungeons');
-    message_intro.react('U+1F6E1');
-    message_intro.react('U+26D1');
-    message_intro.react('U+2694');
-
+    //chan_bot.send('Join queue for MM dungeons');
+    
 });
 
 bot.on ('message', msg =>{
     //test que le bot est fonctionel
     if(msg.content === '!mm'){
         const message_intro = msg.channel.send('Join queue for MM dungeons');
+        console.log(message_intro);
         message_intro.react('U+1F6E1');
         message_intro.react('U+26D1');
         message_intro.react('U+2694');
     }
     //controle sur la file actuel
-    if (dps.length >= 3 && tank.length >= 1 && heal.length >= 1){
+    if (Dps.length >= 3 && Tank.length >= 1 && heal.length >= 1){
         /*msg.reply('le groupe est full a vos clavier : @'
-        +dps[0].username+'#'+dps[0].discriminator
-        +' @'+dps[1].username+'#'+dps[1].discriminator
-        +' @'+dps[2].username+'#'+dps[2].discriminator
-        +' @'+tank[0].username+'#'+tank[0].discriminator
+        +Dps[0].username+'#'+Dps[0].discriminator
+        +' @'+Dps[1].username+'#'+Dps[1].discriminator
+        +' @'+Dps[2].username+'#'+Dps[2].discriminator
+        +' @'+Tank[0].username+'#'+Tank[0].discriminator
         +' @'+heal[0].username+'#'+heal[0].discriminator);*/
-        msg.channel.send(`le groupe est full a vos clavier : ${dps[0]}, ${dps[1]}, ${dps[2]} en tant que DPS , ${tank[0]} en TANK et ${heal[0]} en heal Bonne chance :D`);
+        msg.channel.send(`le groupe est full a vos clavier : ${Dps[0]}, ${Dps[1]}, ${Dps[2]} en tant que DPS , ${Tank[0]} en TANK et ${heal[0]} en heal Bonne chance :D`);
         
-        dps.splice(0,3);
+        Dps.splice(0,3);
         heal.splice(0,1);
-        tank.splice(0,1);
+        Tank.splice(0,1);
     }
     //pour s'ajouter au file d'attente il faut envoyer un messgae +{role} 
     if(msg.content === prefix + 'dps'){
         
-        dps.push(msg.author)
-        msg.reply(dps.length + '/3 dps '+tank.length+'/1 tank '+heal.length+'/1 heal ');
+        Dps.push(msg.author)
+        msg.reply(Dps.length + '/3 Dps '+Tank.length+'/1 Tank '+heal.length+'/1 heal ');
     }
     if(msg.content === prefix + 'tank'){
-        tank.push(msg.author)
-        msg.reply(dps.length + '/3 dps '+tank.length+'/1 tank '+heal.length+'/1 heal ');
+        Tank.push(msg.author)
+        msg.reply(Dps.length + '/3 Dps '+Tank.length+'/1 Tank '+heal.length+'/1 heal ');
     }
     if(msg.content === prefix + 'heal'){
         heal.push(msg.author)
-        msg.reply(dps.length + '/3 dps '+tank.length+'/1 tank '+heal.length+'/1 heal ');
+        msg.reply(Dps.length + '/3 Dps '+Tank.length+'/1 Tank '+heal.length+'/1 heal ');
     }
 
-
 });
-
 
 
 bot.on('messageReactionAdd', (reaction, user) => {
@@ -68,39 +63,31 @@ bot.on('messageReactionAdd', (reaction, user) => {
     if (is_tank != -1){
         if (reaction.emoji.name === ':shield:'){
             if (is_dps != -1){
-
                 dps.splice(is_dps, 1);
             } else if (is_heal != -1){
                 heal.splice(is_heal, 1);
-
             }
             tank.push(u_name);
         }
     } else if (is_dps != -1){
         if (reaction.emoji.name === ':crossed_swords:'){
             if (is_tank != -1){
-
                 tank.splice(is_dps, 1);
             } else if (is_heal != -1){
                 heal.splice(is_heal, 1);
-
             }
             dps.push(u_name);
         }
     } else if (is_heal != -1){
         if (reaction.emoji.name === ':helmet_with_cross:'){
             if (is_tank != -1){
-
                 tank.splice(is_dps, 1);
             } else if (is_dps != -1){
                 dps.splice(is_heal, 1);
-
             }
             heal.push(u_name);
         }
     }
-
 });
-
 
 bot.login('NTU4Njc4NjkwNDE0NzIzMTIy.D3an3Q.RTpLKXqADIH-18d8yS2sRpOmD4Q');
