6051f7b7fa8a40f12279ec2b2cac9911d0bde34a
BejaouiKhalil <35938684+BejaouiKhalil@users.noreply.github.com>
2019-03-22 19:47:30 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ bot.on ('message', msg =>{
     }
     //pour s'ajouter au file d'attente il faut envoyer un messgae +{role} 
     if(msg.content === prefix + 'dps'){
-        console.log(msg);
+        console.log(msg.channel.TextChannel.author.User.username);
         Dps.push(msg.client)
         msg.reply(Dps.length + '/3 Dps '+Tank.length+'/1 Tank '+heal.length+'/1 heal ');
     }
