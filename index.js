5261a2f5f805e4fd6a73a596b4cf7de212995bd1
BejaouiKhalil <35938684+BejaouiKhalil@users.noreply.github.com>
2019-03-22 18:43:59 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.on ('message', msg =>{
         msg.reply('pong!');
     }
     //controle sur la file actuel
-    if (Dps.length() == 3 && Tank.length() == 1 && heal.length() == 1){
+    if (Dps.length == 3 && Tank.length == 1 && heal.length == 1){
         msg.reply('le groupe est full a vos clavier :'+Dps[0].user.username+Dps[1].user.username+Dps[2].user.username+Tank[0].user.username+heal[0].user.username);
         Dps.slice(0,3);
         heal.slice(0,1);
@@ -26,15 +26,15 @@ bot.on ('message', msg =>{
     //pour s'ajouter au file d'attente il faut envoyer un messgae +{role} 
     if(msg.content === prefix + 'dps'){
         Dps.push(msg.client)
-        msg.reply(Dps.length() + '/3 Dps '+Tank.length()+'/1 Tank '+heal.length()+'/1 heal ');
+        msg.reply(Dps.length + '/3 Dps '+Tank.length+'/1 Tank '+heal.length+'/1 heal ');
     }
     if(msg.content === prefix + 'tank'){
         Tank.push(msg.client)
-        msg.reply(Dps.length() + '/3 Dps '+Tank.length()+'/1 Tank '+heal.length()+'/1 heal ');
+        msg.reply(Dps.length + '/3 Dps '+Tank.length+'/1 Tank '+heal.length+'/1 heal ');
     }
     if(msg.content === prefix + 'heal'){
         heal.push(msg.client)
-        msg.reply(Dps.length() + '/3 Dps '+Tank.length()+'/1 Tank '+heal.length()+'/1 heal ');
+        msg.reply(Dps.length + '/3 Dps '+Tank.length+'/1 Tank '+heal.length+'/1 heal ');
     }
 })
-bot.login(process.env.TOKEN);
+bot.login('NTU4Njc4NjkwNDE0NzIzMTIy.D3an3Q.RTpLKXqADIH-18d8yS2sRpOmD4Q');
