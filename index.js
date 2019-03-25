145c6a13a05b7a42b625258ec6c4296bf9610984
Arkanheal <arkanheal@hotmail.fr>
2019-03-25 22:44:05 +0100

    Re-chnaged back to commit

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const discord = require('discord.js');
 const bot = new discord.Client();
 
 const prefix =('+');
-const serverName = 'ahla wa sahla';
-const channelName = 'mythicqueue';
-const emoji_tank = "🛡";
-const emoji_heal = "⛑";
+const serverName = 'BetterWype #Sa';
+const channelName ='mythic-queue';
+var chan_annonce;
+const emoji_tank = "🛡"
+;const emoji_heal ="⛑";
 const emoji_dps = "⚔";
 
 var dps=[];
@@ -13,19 +14,20 @@ var tank=[];
 var heal=[];
 
 bot.on('ready', async ()=> {
-    console.log(process.env.TOKEN);
     console.log(`logged in  as ${bot.user.username} !`);
     //TODO make it dynamic?
     const discord_guild = bot.guilds.find(val => val.name === serverName);
     const chan_bot = discord_guild.channels.find(val => val.name === channelName);
-    chan_bot.send('Join queue for MM dungeons auto')
+    chan_annonce = discord_guild.channels.find(val => val.name === 'mythic-annonce');
+    chan_bot.send('Join queue for MM dungeons')
         .then(async function (message) {
             try{
                 await message.react(emoji_tank);
                 await message.react(emoji_heal);
                 await message.react(emoji_dps);
             }
-            catch (error){
+            catch (error)
+            {
                 console.error('One emoji failed to react');
                 console.error(error);
             }
@@ -33,19 +35,12 @@ bot.on('ready', async ()=> {
 });
 
 bot.on('messageReactionAdd', (reaction, user) => {
-    var u_name = user.username;
+    var u_name = user;
     var is_dps = dps.indexOf(u_name);
     var is_tank = tank.indexOf(u_name);
     var is_heal = heal.indexOf(u_name);
-    if (reaction.message.content === 'Join queue for MM dungeons auto' && !user.bot){
-        //controle sur la file actuel
+    if (reaction.message.content === 'Join queue for MM dungeons' && !user.bot){
         reaction_array = reaction.message.reactions;
-        if (dps.length >= 3 && tank.length >= 1 && heal.length >= 1){
-            reaction.message.channel.send(`Le groupe est full à vos claviers : ${dps[0]}, ${dps[1]}, ${dps[2]} en tant que DPS , ${tank[0]} en TANK et ${heal[0]} en heal Bonne chance :D`);
-            dps.splice(0,3);
-            heal.splice(0,1);
-            tank.splice(0,1);
-        }
         if (reaction.emoji.name === emoji_tank){
             reaction_array.forEach(function (value, index, array){
                 if (value.emoji.name != emoji_tank){
@@ -73,7 +68,7 @@ bot.on('messageReactionAdd', (reaction, user) => {
                     user_array = value.users;
                     user_array.forEach(function (user_react, index, array){
                         if(user_react.username === u_name){
-                            value.remove(user_react)
+                            value.remove(user_react);
                         }
                     });
                 }
@@ -85,7 +80,7 @@ bot.on('messageReactionAdd', (reaction, user) => {
                 tank.splice(is_tank, 1);
             }
             if (is_dps != -1){
-                dps.splice(is_dps, 1);
+                dps.splice(is_dps,1);
             }
         }
         if (reaction.emoji.name === emoji_dps){
@@ -94,7 +89,7 @@ bot.on('messageReactionAdd', (reaction, user) => {
                     user_array = value.users;
                     user_array.forEach(function (user_react, index, array){
                         if(user_react.username === u_name){
-                            value.remove(user_react)
+                            value.remove(user_react);
                         }
                     });
                 }
@@ -109,15 +104,26 @@ bot.on('messageReactionAdd', (reaction, user) => {
                 heal.splice(is_heal, 1);
             }
         }
-    }
-});
+        if (dps.length >= 3 && tank.length >= 1 && heal.length >= 1){
+        chan_annonce.send(`Le groupe est full à vos claviers : ${dps[0]}, ${dps[1]}, ${dps[2]} en tant que DPS , ${tank[0]} en TANK et ${heal[0]} en heal! Bonne chance :D`);
+            reaction_array.forEach(function (value, index, array){
+                value.remove(dps[0]);
+                value.remove(dps[1]);
+                value.remove(dps[2]);
+                value.remove(heal[0]);
+                value.remove(tank[0]);
+            });
+            dps.splice(0,3);
+            heal.splice(0,1);
+            tank.splice(0,1);
+        }}});
 
 bot.on('messageReactionRemove', (reaction, user) => {
     var u_name = user.username;
     var is_dps = dps.indexOf(u_name);
     var is_tank = tank.indexOf(u_name);
     var is_heal = heal.indexOf(u_name);
-    if (reaction.message.content === 'Join queue for MM dungeons auto' && !user.bot){
+    if (reaction.message.content === 'Join queue for MM dungeons' && !user.bot){
         if (reaction.emoji.name === emoji_tank){
             if (is_tank != -1){
                 tank.splice(is_tank, 1);
@@ -135,15 +141,14 @@ bot.on('messageReactionRemove', (reaction, user) => {
         }
     }
 });
-
 //pour voir la file actuel
 bot.on('message', msg => {
     if (msg.content == 'ping'){
         msg.reply('pong');
     }
-    if (msg.content == prefix+'file') {
-        msg.author.send('Dps: '+dps+' Tank: '+tank+' Heal : '+heal);
+    if (msg.content == prefix+'file'){
+        msg.author.send('Dps: '+dps.join('|')+'  // Tank: '+tank.join('|')+'  // Heal : '+heal.join('|'));
     }
 });
 
-bot.login(process.env.TOKEN);
+bot.login("");
