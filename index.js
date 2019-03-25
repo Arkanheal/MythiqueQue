2882d7f3124b156bc1dacdd7e8ad928457ee9fbd
Arkanheal <arkanheal@hotmail.fr>
2019-03-25 18:57:50 +0100

    Added remove when user remove reaction

--- index.js
+++ index.js
@@ -42,12 +42,10 @@ bot.on('messageReactionAdd', (reaction, user) => {
         //controle sur la file actuel
         if (dps.length >= 3 && tank.length >= 1 && heal.length >= 1){
             reaction.message.channel.send(`Le groupe est full à vos claviers : ${dps[0]}, ${dps[1]}, ${dps[2]} en tant que DPS , ${tank[0]} en TANK et ${heal[0]} en heal Bonne chance :D`);
-        
             dps.splice(0,3);
             heal.splice(0,1);
             tank.splice(0,1);
         }
-
         if (reaction.emoji.name === emoji_tank){
             if (is_tank === -1){
                 tank.push(u_name);
@@ -83,7 +81,32 @@ bot.on('messageReactionAdd', (reaction, user) => {
         }
     }
 });
- //pour voir la file actuel
+
+bot.on('messageReactionRemove', (reaction, user) => {
+    var u_name = user.username;
+    var is_dps = dps.indexOf(u_name);
+    var is_tank = tank.indexOf(u_name);
+    var is_heal = heal.indexOf(u_name);
+    if (reaction.message.content === 'Join queue for MM dungeons auto' && !user.bot){
+        if (reaction.emoji.name === emoji_tank){
+            if (is_tank != -1){
+                tank.splice(is_tank, 1);
+            }
+        }
+        if (reaction.emoji.name === emoji_heal){
+            if (is_heal != -1){
+                heal.splice(is_heal, 1);
+            }
+        }
+        if (reaction.emoji.name === emoji_dps){
+            if (is_dps != -1){
+                dps.splice(is_dps, 1);
+            }
+        }
+    }
+});
+
+//pour voir la file actuel
 bot.on('message', msg => {
     if (msg.content == 'ping'){
         msg.reply('pong');
