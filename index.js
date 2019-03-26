5dc5d2903eba8d775637ab042e69554f2d337082
pierre <pierre.savigny@abc-culture.fr>
2019-03-26 11:22:17 +0100

    Changed the way removing reaction is handled

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ bot.on('messageReactionAdd', (reaction, user) => {
                 if (value.emoji.name != emoji_tank){
                     user_array = value.users;
                     user_array.forEach(function (user_react, index, array){
-                        if(user_react === u_name){
+                        if(user_react.username === u_name.username){
                             value.remove(user_react)
                         }
                     });
@@ -67,7 +67,7 @@ bot.on('messageReactionAdd', (reaction, user) => {
                 if (value.emoji.name != emoji_heal){
                     user_array = value.users;
                     user_array.forEach(function (user_react, index, array){
-                        if(user_react === u_name){
+                        if(user_react.username === u_name.username){
                             value.remove(user_react);
                         }
                     });
@@ -88,7 +88,7 @@ bot.on('messageReactionAdd', (reaction, user) => {
                 if (value.emoji.name != emoji_dps){
                     user_array = value.users;
                     user_array.forEach(function (user_react, index, array){
-                        if(user_react === u_name){
+                        if(user_react.username === u_name.username){
                             value.remove(user_react);
                         }
                     });
